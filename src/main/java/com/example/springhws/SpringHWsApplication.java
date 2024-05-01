4a480fe26c6030406e315e3273f31be564d3b9e4
Stanislav <stdu92@mail.ru>
2024-05-01 15:38:31 +0500

    default

---
+++ src/main/java/com/example/springhws/SpringHWsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHWsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springhws/SpringHWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
